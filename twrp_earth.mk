bed864eb4ab9e9797fd929a9054f560eb0531e83
Kevin chinedu <kelvinchinedu97@gmail.com>
2025-07-28 17:36:18 +0100

    Update twrp_earth.mk

--- twrp_earth.mk
+++ twrp_earth.mk
@@ -1,6 +1,5 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from earth device.
 $(call inherit-product, device/xiaomi/earth/device.mk)
