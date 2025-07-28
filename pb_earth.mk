30060057774b9845b91bcbf5541996b632c075dc
Kevin chinedu <kelvinchinedu97@gmail.com>
2025-07-28 19:17:56 +0100

    Update pb_earth.mk

--- pb_earth.mk
+++ pb_earth.mk
@@ -1,14 +1,14 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from earth device.
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
-# Inherit some common TWRP stuff.
+# Inherit some common PBRP stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
 
 # Product Specifics
