95fe6db41a7ee79031319dbf6c77b76e7817d54a
Kevin chinedu <kelvinchinedu97@gmail.com>
2025-07-28 21:17:30 +0100

    Update twrp_earth.mk

--- twrp_earth.mk
+++ twrp_earth.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
 # Inherit from earth device
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
