4fccb4c344805ddabd8649b05320a5c30f796442
Kevin chinedu <kelvinchinedu97@gmail.com>
2025-07-28 17:21:50 +0100

    Update and rename pb_earth.mk to twrp_earth.mk

--- twrp_earth.mk
+++ twrp_earth.mk
@@ -2,9 +2,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Inherit from earth device.
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
@@ -12,7 +9,7 @@ $(call inherit-product, device/xiaomi/earth/device.mk)
 $(call inherit-product, vendor/pb/config/common.mk)
 
 # Product Specifics
-PRODUCT_NAME := pb_earth
+PRODUCT_NAME := twrp_earth
 PRODUCT_DEVICE := earth
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
