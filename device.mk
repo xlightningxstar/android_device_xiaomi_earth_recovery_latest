3bd5960d90e75d29b82f4b456e5d78ce5b5d8ca2
Kevin chinedu <kelvinchinedu97@gmail.com>
2025-07-28 17:37:13 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -8,6 +8,9 @@ TARGET_SCREEN_HEIGHT := 1650
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
