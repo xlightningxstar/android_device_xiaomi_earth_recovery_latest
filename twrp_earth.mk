a1ddf805d02612d55cfad55c69d4b735f9e9d54d
Kevin chinedu <kelvinchinedu97@gmail.com>
2025-07-28 18:25:02 +0100

    Update twrp_earth.mk

--- twrp_earth.mk
+++ twrp_earth.mk
@@ -1,3 +1,13 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from this product for devices that support only 64-bit apps using:
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
 # Inherit from earth device.
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
